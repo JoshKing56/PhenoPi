cf60639fb9f20e31f1fb17d875d12d5e64f24a35
Josh King <joshuaiking56@gmail.com>
2019-05-18 19:17:50 -0700

    adding set_config_var. See if this works.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,26 @@
 # This enables the camera. Pulled from the source code of raspi-config
+#
+set_config_var() {
+lua - "$1" "$2" "$3" <<EOF > "$3.bak"
+local key=assert(arg[1])
+local value=assert(arg[2])
+local fn=assert(arg[3])
+local file=assert(io.open(fn))
+local made_change=false
+for line in file:lines() do
+  if line:match("^#?%s*"..key.."=.*$") then
+    line=key.."="..value
+    made_change=true
+  end
+  print(line)
+end
+
+if not made_change then
+  print(key.."="..value)
+end
+EOF
+mv "$3.bak" "$3"
+}
 
 # Stop if /boot is not a mountpoint
 if ! mountpoint -q /boot; then
@@ -14,3 +36,5 @@ if [ -z "$CUR_GPU_MEM" ] || [ "$CUR_GPU_MEM" -lt 128 ]; then
 fi
 sed /boot/config.txt -i -e "s/^startx/#startx/"
 sed /boot/config.txt -i -e "s/^fixup_file/#fixup_file/"
+
+
