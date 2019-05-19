4085ce03fb5c1dc92a64a803c1695bed44d3c7e1
Josh King <joshuaiking56@gmail.com>
2019-05-18 21:17:36 -0700

    removed exposure test, adding fp and waittime as constants

--- camera.py
+++ camera.py
@@ -1,17 +1,21 @@
 from picamera import PiCamera
 from time import sleep
+from datetime import datetime
 
 camera = PiCamera()
 
-def captureImage(waitTime, savePath):
+waitTime = 2
+outDirectory =  "/home/pi/Pictures/plant_data/"
+
+def captureImage():
+    timeStamp = str(datetime.now()) #TODO: Make this better for capturing
+    outFile = outDirectory + timeStamp + ".png"
     camera.start_preview()
     # need to sleep for at least two seconds to make sure sensors adjust
     # TODO: test to see if this is right
     # TODO: pass this value as a param
     sleep(waitTime)
-    camera.capture(savePath)
+    camera.capture(outDirectory)
     camera.stop_preview()
 
-for i in range(10):
-    captureImage(i, "/home/pi/Pictures/plant_data/%s.png" % i)
-
+captureImage()
