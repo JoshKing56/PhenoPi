82d17d9a899f7f94dabe747ddf320df96fe5a35e
Josh King <joshuaiking56@gmail.com>
2019-05-31 18:34:55 -0700

    Added wtf-python form to control backend

--- camera.py
+++ camera.py
@@ -9,10 +9,6 @@ camera = PiCamera()
 waitTime = 2
 remoteName = "gdrive"
 
-user = "Josh"
-species = "Barley"
-plant = 5
-
 def setupLocalDir(parentDir, timeStamp):
     print(parentDir)
     if not os.path.exists(parentDir):
@@ -24,7 +20,8 @@ def setupLocalDir(parentDir, timeStamp):
             os.makedirs(fullPath)
             return(fullPath + "/")
 
-def captureImage(outDirectory, timeStamp):
+
+def takePicture(outDirectory, timeStamp):
     outFile = outDirectory + timeStamp + ".png"
     camera.start_preview()
     # need to sleep for at least two seconds to make sure sensors adjust
@@ -35,22 +32,30 @@ def captureImage(outDirectory, timeStamp):
     camera.stop_preview()
     return outFile
 
-def generateMetaData(timeStamp, filename):
-    output = {'User': user, "Plant species": species, "Plant Number": plant, "Picture taken": timeStamp}
+
+def generateMetaData(metadata, timeStamp, filename):
+    #output = {'User': user, "Plant species": species, "Plant Number": plant, "Picture taken": timeStamp}
+    metadata["Picture taken"] = timeStamp
     with open(filename, 'w') as outfile:
-        yaml.dump(output, outfile, default_flow_style=False)
+        yaml.dump(metadata, outfile, default_flow_style=False)
+
 
 def uploadData(source, dest):
-    rclone = "rclone copy " + source + " " + remoteName + ":" + dest 
+    rclone = "rclone copy " + source + " " + remoteName + ":" + dest
     os.system(rclone)
 
-outDirectory = timeStamp = str(datetime.now().strftime('%Y-%m-%d_%H_%M_%S')) #TODO: Make this better for capturing
-print(timeStamp)
 
-localDir = setupLocalDir("/home/pi/Pictures/plant_data", timeStamp)
-infoPath = localDir + 'info.yml'
-generateMetaData(timeStamp, infoPath)
-imagePath = captureImage(localDir, timeStamp)
+def captureImage(metadata):
+    outDirectory = timeStamp = str(datetime.now().strftime(
+        '%Y-%m-%d_%H_%M_%S'))  # TODO: Make this better for capturing
+    print(timeStamp)
 
-remoteDir = "Phenotyping/plant_data/" + timeStamp
-uploadData(localDir, remoteDir)
+    localDir = setupLocalDir("/home/pi/Pictures/plant_data", timeStamp)
+    infoPath = localDir + 'info.yml'
+    generateMetaData(metadata, timeStamp, infoPath)
+    imagePath = takePicture(localDir, timeStamp)
+
+    remoteDir = "Phenotyping/plant_data/" + timeStamp
+    uploadData(localDir, remoteDir)
+
+    return(remoteDir)
--- display.py
+++ display.py
@@ -1,11 +1,32 @@
 from flask import Flask, render_template
+from flask_wtf import Form
+from wtforms import TextField, SubmitField
+from wtforms import validators, ValidationError
+import camera
 
 app = Flask(__name__)
 
-@app.route("/")
+class MetaDataForm(Form):
+    user = TextField("User name",[validators.Required("Please enter name")], default="Josh") 
+    species = TextField("Species of plant", default = "Barley")
+    plant = TextField("ID of the plant", default = "1")
+    experiment = TextField("Experiment name/number", default = "PhenoPi rig testing")
+    submit = SubmitField("Capture picture")
 
+@app.route("/", methods=['GET', 'POST'])
 def index():
-   return render_template('display.html') 
+   form = MetaDataForm()
+
+if request.method == 'POST':
+    if form.validate_on_submit() == False:
+        flash('All fields are required.')
+        return render_template('display.html', form = form)
+    else:
+        metadata = {'User': form.user.data, "Plant species": form.species.data, "Plant Number": form.plant.data}
+        result = camera.captureImage(metadata) #TODO: Make a trycatch here
+        return render_template('display.html')
+    elif request.method == 'GET':
+        return render_template('display.html', form = form)
 
 if __name__ == "__main__":
     app.run(debug=True)
