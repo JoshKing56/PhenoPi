28e4923aba575ecd127635736867a3f20edbc789
Josh King <joshuaiking56@gmail.com>
2019-05-31 19:50:27 -0700

    Fixed shit

--- display.py
+++ display.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, flash, request
+from flask import Flask, render_template, flash, request, redirect
 from flask_wtf import Form
 from wtforms import TextField, SubmitField
 from wtforms import validators, ValidationError
-import camera
+import os
+# import camera
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(32)
 
 class MetaDataForm(Form):
     user = TextField("User name",[validators.Required("Please enter name")], default="Josh") 
@@ -16,17 +18,16 @@ class MetaDataForm(Form):
 @app.route("/", methods=['GET', 'POST'])
 def index():
     form = MetaDataForm()
+    name = ""
+    if request.method =="POST":
+        metadata = {'User': form.user.data, "Plant species": form.species.data, "Plant Number": form.plant.data}
+        result = camera.captureImage(metadata) #TODO: Make a trycatch here
+    return render_template('display.html', form = form)
 
-    if request.method == 'POST':
-        if form.validate_on_submit() == False:
-            flash('All fields are required.')
-            return render_template('display.html', form = form)
-        else:
-            metadata = {'User': form.user.data, "Plant species": form.species.data, "Plant Number": form.plant.data}
-            result = camera.captureImage(metadata) #TODO: Make a trycatch here
-            return render_template('display.html')
-    elif request.method == 'GET':
-        return render_template('display.html', form = form)
+@app.route("/picture")
+def takePicture():
+    return render_template('success.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
